406598d62966a3e800a183aa840183d95df82f47
ebeb0001 <ethan.bebelambou.nkoli@ulb.be>
2025-11-08 23:48:35 +0100

    can see the convex hull on the canvas

--- js/geometry.js
+++ js/geometry.js
@@ -1,71 +1,48 @@
 export class Point {
-	constructor(x, y) {
+	constructor(x, y, radius = 8) {
 		this.x = x;
 		this.y = y;
+		this.radius = radius;
+	}
+
+	draw() {
+		circle(this.x, this.y, this.radius);
 	}
 }
 
-export class Polygon {
-	constructor(points) {
-		this.points = points;
-		this.sortPoints();
+export function drawPolygon(points) {
+	beginShape();
+	for (const p of points) {
+		vertex(p.x, p.y);
 	}
-
-	nb_points() {
-		return this.points.length;
-	}
-
-	get_point(i) {
-		if (i < this.nb_points()) {
-			return this.points[i];
-		}
-	}
-
-	sortPoints() {
-		let p0 = this.points[0];
-		for (let i = 1; i < this.points.length; i++) {
-			if (this.points[i].x < p0.x || 
-			(this.points[i].x === p0.x && this.points[i].y > p0.y)) {
-			p0 = this.points[i];
-			}
-		}
-
-		const rest = this.points.filter((p) => p !== p0);
-		rest.sort(cmpDist.bind(null, p0));
-
-		return [p0, ...rest];
-	}
-
-	index(p) {
-		return this.points.findIndex((q) => q.x === p.x && q.y === p.y);
-	}
+	endShape(CLOSE);
 }
 
-export class Triangle {
-	constructor(a, b, c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
+// export class Triangle {
+// 	constructor(a, b, c) {
+// 		this.a = a;
+// 		this.b = b;
+// 		this.c = c;
+// 	}
 
-	orientVerification(o1, o2) {
-		return (
-			(isLeftTurn(o1) && isLeftTurn(o2)) || 
-			(isRightTurn(o1) && isRightTurn(o2))
-		);
-	}
+// 	orientVerification(o1, o2) {
+// 		return (
+// 			(isLeftTurn(o1) && isLeftTurn(o2)) || 
+// 			(isRightTurn(o1) && isRightTurn(o2))
+// 		);
+// 	}
 
-	isInside(p) {
-		var orient_ab = orient(this.a, this.b, p);
-		var orient_bc = orient(this.b, this.c, p);
-		var orient_ac = orient(this.c, this.a, p);
-		return (
-			this.orientVerification(orient_ab, orient_bc) &&
-			this.orientVerification(orient_bc, orient_ac) &&
-			this.orientVerification(orient_ac, orient_ab)
-		);
-	}
-}
+// 	isInside(p) {
+// 		var orient_ab = orient(this.a, this.b, p);
+// 		var orient_bc = orient(this.b, this.c, p);
+// 		var orient_ac = orient(this.c, this.a, p);
+// 		return (
+// 			this.orientVerification(orient_ab, orient_bc) &&
+// 			this.orientVerification(orient_bc, orient_ac) &&
+// 			this.orientVerification(orient_ac, orient_ab)
+// 		);
+// 	}
+// }
 
 export function orient(A, B, C) {
 	const det = (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
@@ -76,34 +53,34 @@ export function isLeftTurn(orient_det) { return orient_det < 0; }
 
 export function isRightTurn(orient_det) { return orient_det > 0; }
 
-export function dist(p1, p2) {
-	const distance = (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2;
-	return distance;
+export function dist(p, q) {
+	return Math.sqrt((p.x - q.x) ** 2 + (p.y - q.y) ** 2);
 }
 
-export function cmpDist(p, p1, p2) {
-	const orientation = orient(p, p1, p2);
-	if (orientation > 0) { return -1;} 
-	else if (orientation < 0) { return 1; }
-
-	dist_p_p1 = dist(p, p1);
-	dist_p_p2 = dist(p, p2);
-
-	if (dist_p_p1 > dist_p_p2) { return -1; } 
-	else if (dist_p_p1 < dist_p_p2) { return 1; } 
-	else { return 0; }
+export function sortPoints(points) {
+	points = points.slice().sort((p, q) => (p.y - q.y || p.x - q.x));
+	const p0 = points[0];
+	const rest = points.filter((p) => p !== p0);
+	rest.sort((p, q) => {
+		const orient_p = orient(p0, p, q);
+		if (orient_p !== 0) { return -orient_p; }
+		return dist(p0, p) - dist(p0, q);
+	});
+	return [p0, ...rest];
 }
 
-export function convexHullGrahamScan(polygon) {
-	p0 = polygon.get_point(0);
+export function convexHullGrahamScan(points) {
+	console.log('points:', points);
+	points = sortPoints(points);
+	// console.log('Sorted points:', points);
 	var hull = [];
 
-	for (p of polygon.points) {
+	for (const p of points) {
 		while (hull.length >= 2 &&
-		orient(hull[hull.length - 2], hull[hull.length - 1], p) <= 0) {
+		isLeftTurn(orient(hull[hull.length - 2], hull[hull.length - 1], p))) {
 			hull.pop();
 		}
 		hull.push(p);
 	}
-	return new Polygon(hull);
+	return hull;
 }
--- js/playground.js
+++ js/playground.js
@@ -1,151 +1,210 @@
 import * as geometry from './geometry.js';
 
 const state = {
-    k: 2,
-    points: [],
-    mode: 'view', // 'view' | 'add' | 'drag' | 'delete'
-    showHull: true,
-    showLabels: false,
-    history: [],
-    future: [],
-    polygons : [],
-    hull : null
+	k: 2,
+	points: [],
+	mode: 'add', // 'view' | 'add' | 'drag' | 'delete'
+	showHull: false,
+	showLabels: false,
+	history: [],
+	future: [],
+	polygons : [],
+	hull : null
 };
 
 let canvas;
 let dragIndex = -1;
 
 function setRandomPreset(nb_points) {
-    const points = [];
-    for (let i = 0; i < nb_points; i++) {
-        points.push(new geometry.Point(
-            Math.floor(Math.random() * (width + 1)), 
-            Math.floor(Math.random() * (height + 1))
-        ));
-    }
-    return points;
+	const points = [];
+	for (let i = 0; i < nb_points; i++) {
+		points.push(new geometry.Point(
+			Math.floor(Math.random() * (width + 1)), 
+			Math.floor(Math.random() * (height + 1))
+		));
+	}
+	return points;
+}
+
+function clearCanvas() {
+	state.points = [];
+	state.history = [];
+	state.future = [];
+	state.hull = null;
+	state.showHull = false;
+	state.showLabels = false;
+	setCanvasMode('add');
+	redrawAndStats();
 }
 
 const $ = (id) => document.getElementById(id);
 window.addEventListener('load', () => {
-    $("year").textContent = new Date().getFullYear();
+	$("year").textContent = new Date().getFullYear();
 
-    $('kRange').addEventListener('input', (e) => {
-        state.k = parseInt(e.target.value, 10);
-        $('kValue').textContent = state.k;
-        $('stat-k').textContent = state.k;
-    });
+	$('kRange').addEventListener('input', (e) => {
+		state.k = parseInt(e.target.value, 10);
+		$('kValue').textContent = state.k;
+		$('stat-k').textContent = state.k;
+	});
 
-    $('mode-view').addEventListener('click', () => setMode('view'));
-    $('mode-edit').addEventListener('click', () => setMode('add'));
-    $('mode-drag').addEventListener('click', () => setMode('drag'));
-    $('mode-delete').addEventListener('click', () => setMode('delete'));
+	$('mode-view').addEventListener('click', () => setCanvasMode('view'));
+	$('mode-edit').addEventListener('click', () => setCanvasMode('add'));
+	$('mode-drag').addEventListener('click', () => setCanvasMode('drag'));
+	$('mode-delete').addEventListener('click', () => setCanvasMode('delete'));
 
-    $('btn-random').addEventListener('click', () => { 
-        state.points = setRandomPreset(Math.floor(Math.random() * 41)); 
-        redrawAndStats(); 
-    });
+	$('btn-random').addEventListener('click', () => { 
+		state.points = setRandomPreset(Math.floor(Math.random() * 41));
+		setPrimaryButton('rand');
+		redrawAndStats(); 
+	});
+
+	$('btn-k-out').addEventListener('click', () => {
+		setPrimaryButton('k-out');
+		redrawAndStats();
+	});
+
+	$('btn-undo').addEventListener('click', () => { 
+		setPrimaryButton('undo');
+		redrawAndStats(); 
+	});
+
+	$('btn-redo').addEventListener('click', () => { 
+		setPrimaryButton('redo');
+		redrawAndStats(); 
+	});
+
+	$('btn-clear').addEventListener('click', () => { 
+		setPrimaryButton('clear');
+		clearCanvas(); 
+		redrawAndStats(); 
+	});
+
+	$('hull').addEventListener('change', (e) => { 
+		state.showHull = e.target.checked; 
+		redrawAndStats();
+	});
 });
 
-function setMode(m) {
-    state.mode = m;
-    for (const elem of document.querySelectorAll('.seg-btn')) {
-        elem.classList.remove('active');
-    }
+function setPrimaryButton(button) {
+	for (const elem of document.querySelectorAll('.btn')) {
+		elem.classList.remove('primary');
+	}
 
-    if (m === 'view') $('mode-view').classList.add('active');
-    if (m === 'add') $('mode-edit').classList.add('active');
-    if (m === 'drag') $('mode-drag').classList.add('active');  
-    if (m === 'delete') $('mode-delete').classList.add('active');
+	if (button === 'rand') $('btn-random').classList.add('primary');
+	else if (button === 'k-out') $('btn-k-out').classList.add('primary');
+	else if (button === 'undo') $('btn-undo').classList.add('primary');  
+	else if (button === 'redo') $('btn-redo').classList.add('primary');
+	else if (button === 'clear') $('btn-clear').classList.add('primary');
+}
+
+function setCanvasMode(mode) {
+	state.mode = mode;
+	for (const elem of document.querySelectorAll('.seg-btn')) {
+		elem.classList.remove('active');
+	}
+
+	if (mode === 'view') $('mode-view').classList.add('active');
+	else if (mode === 'add') $('mode-edit').classList.add('active');
+	else if (mode === 'drag') $('mode-drag').classList.add('active');  
+	else if (mode === 'delete') $('mode-delete').classList.add('active');
 }
 
 function setup() {
-    const container = document.getElementById('canvas-container');
-    canvas = createCanvas(container.clientWidth, container.clientHeight);
-    canvas.parent('canvas-container');
-    noLoop(); // draw on change
+	const container = document.getElementById('canvas-container');
+	canvas = createCanvas(container.clientWidth, container.clientHeight);
+	canvas.parent('canvas-container');
+	noLoop(); // draw on change
 }
 
 function windowResized() {
-    const container = document.getElementById('canvas-container');
-    resizeCanvas(container.clientWidth, container.clientHeight);
-    redraw();
+	const container = document.getElementById('canvas-container');
+	resizeCanvas(container.clientWidth, container.clientHeight);
+	redraw();
 }
 
 function draw() {
-    background(255);
-    noFill();
-    
-    stroke(150);
-    for (let y = 0; y < height; y += 40) { line(0, y, width, y); }
-    for (let x = 0; x < width; x += 40) { line(x, 0, x, height); }
+	background(255);
+	noFill();
+	
+	stroke(150);
+	for (let y = 0; y < height; y += 40) { line(0, y, width, y); }
+	for (let x = 0; x < width; x += 40) { line(x, 0, x, height); }
 
-    noStroke();
-    for (const p of state.points) {
-        fill(24);
-        circle(p.x, p.y, 8);
-    }
+	if (state.showHull && state.hull) {
+		stroke(50, 125,240);
+		strokeWeight(2);
+		noFill();
+		geometry.drawPolygon(state.hull);
+	}
+
+	strokeWeight(1);
+	noStroke();
+	for (const p of state.points) {
+		fill(24);
+		p.draw();
+	}
+
 }
 
 function mousePressed() {
-    const insideCanvas = mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height;
-    if (!insideCanvas) return;
-    if (state.mode === 'add') {
-        state.points.push(new geometry.Point(mouseX, mouseY));
-        redrawAndStats();
-    } else if (state.mode === 'drag') {
-        dragIndex = findNearestPoint(mouseX, mouseY);
-    } else if (state.mode === 'delete') {
-        const idx = findNearestPoint(mouseX, mouseY);
-        if (idx !== -1) {
-            state.points.splice(idx, 1);
-            redrawAndStats();
-        }
-    }
+	const insideCanvas = mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height;
+	if (!insideCanvas) return;
+	if (state.mode === 'add') {
+		state.points.push(new geometry.Point(mouseX, mouseY));
+		redrawAndStats();
+	} else if (state.mode === 'drag') {
+		dragIndex = findNearestPoint(mouseX, mouseY);
+	} else if (state.mode === 'delete') {
+		const idx = findNearestPoint(mouseX, mouseY);
+		if (idx !== -1) {
+			state.points.splice(idx, 1);
+			redrawAndStats();
+		}
+	}
 }
 
 function mouseDragged() {
-    if (state.mode === 'drag' && dragIndex !== -1) {
-        state.points[dragIndex].x = mouseX;
-        state.points[dragIndex].y = mouseY;
-        redrawAndStats();
-    }
+	if (state.mode === 'drag' && dragIndex !== -1) {
+		state.points[dragIndex].x = mouseX;
+		state.points[dragIndex].y = mouseY;
+		redrawAndStats();
+	}
 }
 
 function mouseReleased() {
-    if (state.mode === 'drag' && dragIndex !== -1) {
-        redrawAndStats();
-        dragIndex = -1;
-    }
+	if (state.mode === 'drag' && dragIndex !== -1) {
+		redrawAndStats();
+		dragIndex = -1;
+	}
 }
 
 function updateStats() {
-    document.getElementById('stat-n').textContent = state.points.length;
-    document.getElementById('stat-k').textContent = state.k;
-    if (state.points.length >= 3) {
-        // state.hull = geometry.convexHull(new Polygon(state.points));
-    }
-    document.getElementById('stat-h').textContent = state.hull ? state.hull.points.length : 0;
+	document.getElementById('stat-n').textContent = state.points.length;
+	document.getElementById('stat-k').textContent = state.k;
+	if (state.points.length >= 3) {
+		state.hull = geometry.convexHullGrahamScan(state.points);
+	} else { state.hull = null; }
+	document.getElementById('stat-h').textContent = state.hull ? state.hull.length : 0;
 }
 
 function redrawAndStats() {
-    redraw();
-    updateStats();
+	updateStats();
+	redraw();
 }
 
 function findNearestPoint(x, y, r = 12) {
-    let idx = -1, best = r * r;
-    for (let i = 0; i < state.points.length; i++) {
-        const p = state.points[i];
-        const dx = p.x - x;
-        const dy = p.y - y;
-        const dist2 = dx * dx + dy * dy;
-        if (dist2 < best) {
-            best = dist2;
-            idx = i;
-        }
-    }
-    return idx;
+	let idx = -1, best = r * r;
+	for (let i = 0; i < state.points.length; i++) {
+		const p = state.points[i];
+		const dx = p.x - x;
+		const dy = p.y - y;
+		const dist2 = dx * dx + dy * dy;
+		if (dist2 < best) {
+			best = dist2;
+			idx = i;
+		}
+	}
+	return idx;
 }
 
 window.setup = setup;
