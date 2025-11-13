43cf3574d6722c601bdd07d68fa4fb4e93754208
ebeb0001 <ethan.bebelambou.nkoli@ulb.be>
2025-11-13 23:54:18 +0100

    small changes in enumerator.js

--- js/enumerator.js
+++ js/enumerator.js
@@ -21,7 +21,8 @@ export function findChildren(points, polygon, p_j, embeddable_vertices, children
 			const inside_points = geometry.insideOutsidePoints(points, polygon);
 			for (const p of inside_points) {
 				if (geometry.isActive(p_i, p, null, polygon, outside_points, points, k)) {
-					child = geometry.dig(p_i, p, polygon, points)
+					child = geometry.dig(p_i, p, polygon, points);
+					outside_points = geometry.insideOutsidePoints(points, polygon, false);
 					embeddable_vertices = geometry.embeddableVertices(child, outside_points);
 					findChildren(points, child, p_i, embeddable_vertices, children, k);
 				}
@@ -31,6 +32,7 @@ export function findChildren(points, polygon, p_j, embeddable_vertices, children
 	for (const p_i of polygon) {
 		if (geometry.isActive(null, null, p_i, polygon, outside_points, points, k)) {
 			child = geometry.rmv(polygon, p_i, outside_points, points, k);
+			outside_points = geometry.insideOutsidePoints(points, polygon, false);
 			embeddable_vertices = geometry.embeddableVertices(child, outside_points);
 			findChildren(points, child, null, embeddable_vertices, children, k);
 		}
